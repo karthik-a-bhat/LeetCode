7859be1aa0fd2266bfcc4bd6441878a90083b24f
Karthik Bhat <bhatkarthik1996@gmail.com>
2020-09-18 14:44:23 -0700

    Create 0314_Binary_Tree_Vertical_Order_Traversal.java

---
+++ Java/0314_Binary_Tree_Vertical_Order_Traversal.java
@@ -0,0 +1,65 @@
+//https://leetcode.com/problems/binary-tree-vertical-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        
+        List<List<Integer>> res = new ArrayList();
+        
+        TreeMap<Integer, List<Integer>> treeMap = new TreeMap();
+        
+        
+        if(root==null) return res;
+        
+        Queue<TreeNode> q1 = new LinkedList();
+        Queue<Integer> q2 = new LinkedList();
+        
+        q1.add(root); q2.add(0);
+        
+        
+        while(!q1.isEmpty()){
+            
+            int len = q1.size();
+            
+            for(int i=0;i<len;i++){
+                
+                TreeNode curr = q1.poll();
+                int col = q2.poll();
+                
+                List<Integer> tempList = treeMap.getOrDefault(col, new ArrayList());
+                
+                tempList.add(curr.val);
+                
+                treeMap.put(col,tempList);
+                
+                if(curr.left!=null){
+                    q1.add(curr.left);
+                    q2.add(col-1);
+                }
+                if(curr.right!=null){
+                    q1.add(curr.right);
+                    q2.add(col+1);
+                }
+                
+            }
+            
+        }
+        
+        res.addAll(treeMap.values());
+        return res;
+    }
+}
