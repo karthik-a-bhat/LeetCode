f3b23d429a1d45d83ae7cfae20476044642ce5cd
Karthik Bhat <bhatkarthik1996@gmail.com>
2020-10-15 09:29:15 -0700

    Create 0116_Populating_Next_Right_Pointers_in_Each_Node.java

---
+++ Java/0116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        Node curr = root;
+        
+        while(curr!=null){
+            
+            Node level_start = curr;
+            
+            while(curr!=null){
+                
+                if(curr.left!=null) curr.left.next = curr.right;
+                
+                if(curr.next!=null && curr.right!=null) curr.right.next = curr.next.left;
+                
+                curr=curr.next;
+            }
+            
+            curr=level_start.left;
+        }
+        return root;
+    }
+}
