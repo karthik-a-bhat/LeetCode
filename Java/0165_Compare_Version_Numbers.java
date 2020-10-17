00bacf7d71d2ed44e42d5bdb495de2281ae99fa1
Karthik Bhat <bhatkarthik1996@gmail.com>
2020-10-17 14:07:54 -0700

    Create 0165_Compare_Version_Numbers.java

---
+++ Java/0165_Compare_Version_Numbers.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/compare-version-numbers/
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        
+        int i=0;
+        
+        while(i<v1.length || i<v2.length){
+            
+            
+            if(i<v1.length && i<v2.length){
+                
+                
+                if(Integer.parseInt(v1[i])<Integer.parseInt(v2[i])){
+                    return -1;
+                }
+                else if(Integer.parseInt(v1[i])>Integer.parseInt(v2[i])){
+                    return 1;
+                }
+                else{
+                    i++;
+                }
+                
+            }
+            else if(i<v1.length){
+                
+                if(Integer.parseInt(v1[i])!=0) return 1;
+                i++;
+            }
+            else if(i<v2.length){
+                if(Integer.parseInt(v2[i])!=0) return -1;
+                i++;
+            }
+        
+        }
+        return 0;
+    }
+}
