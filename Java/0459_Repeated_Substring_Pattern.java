41fd89aee9796aaa6097df9a02117a1781a9853c
Karthik Bhat <bhatkarthik1996@gmail.com>
2020-11-29 19:21:51 -0800

    Create 0459_Repeated_Substring_Pattern.java

---
+++ Java/0459_Repeated_Substring_Pattern.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/repeated-substring-pattern/
+
+class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        String s = str + str;
+        return s.substring(1, s.length() - 1).contains(str);
+    }
+}
